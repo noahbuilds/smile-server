f5172480311c272b0dddd77b36ea5b249a735a45
Noah Edet <edetnoah@gmail.com>
2025-07-18 11:27:57 +0100

    fix(): add wildcard

--- src/app.ts
+++ src/app.ts
@@ -9,4 +9,7 @@ app.use(cors());
 app.use(express.json({ limit: "5mb" }));
 
 app.use("/api/v1/liveness", livenessRoutes);
+app.get("*", (req, res) => {
+  res.status(404).json({ message: "Route not found" });
+});
 app.use(errorHandler);
